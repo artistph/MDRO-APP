e332190aa7cc39576ada17a710316c5503add0fc
artistph <ahmednumber199@gmail.com>
2026-05-09 10:13:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,32 @@
-const CACHE = 'mdro-v7';
-const BASE = 'https://artistph.github.io/MDRO-APP';
+const CACHE = 'mdro-v4';
+const CACHE = 'mdro-v5';
 const FILES = [
-  BASE + '/',
-  BASE + '/index.html',
-  BASE + '/manifest.json',
-  BASE + '/icon-192.png',
-  BASE + '/icon-512.png'
+  'https://artistph.github.io/MDRO-APP/',
+  'https://artistph.github.io/MDRO-APP/index.html',
+  'https://artistph.github.io/MDRO-APP/manifest.json',
+  'https://artistph.github.io/MDRO-APP/icon-192.png',
+  'https://artistph.github.io/MDRO-APP/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(FILES))
-      .catch(err => console.log('Cache error:', err))
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ])).catch(() => {})
   );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
+@@ -24,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
