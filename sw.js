bb4bf7f86263dedca45c3f08d5eb563ecdf719a3
artistph <ahmednumber199@gmail.com>
2026-05-17 23:26:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // تعليمات الترقية: غيّر CACHE_VERSION فقط عند كل إصدار جديد
 // ═══════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'mdro-v69';
+const CACHE_VERSION = 'mdro-v70';
 const CACHE = CACHE_VERSION;
 
 const CORE_FILES = [
