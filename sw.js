0062a43d2e790cdf77bc5bced9e99730d499308a
artistph <ahmednumber199@gmail.com>
2026-09-08 22:47:52 +0300

    Nudge hospital line up another 2px on mobile (sw v155)

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 // â”€â”€ Caching â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-const CACHE_VERSION = 'mdro-v154';
+const CACHE_VERSION = 'mdro-v155';
 const CACHE = CACHE_VERSION;
 
 const CORE_FILES = [
