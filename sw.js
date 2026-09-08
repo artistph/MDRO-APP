23f8e4eeb2706daf4d4bbbee4a9a412915ec2c89
artistph <ahmednumber199@gmail.com>
2026-09-08 20:51:52 +0300

    Header order/title-font tweaks + conn chip متصل (sw v141)

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 // â”€â”€ Caching â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-const CACHE_VERSION = 'mdro-v140';
+const CACHE_VERSION = 'mdro-v141';
 const CACHE = CACHE_VERSION;
 
 const CORE_FILES = [
