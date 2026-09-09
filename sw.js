bd98add79454512b52c6836a7a2bab8bb7855bc5
artistph <ahmednumber199@gmail.com>
2026-09-09 18:13:47 +0300

    لون أغمق وتنعيم حواف زر القائمة

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 // ── Caching ────────────────────────────────────────────────
-const CACHE_VERSION = 'mdro-v179';
+const CACHE_VERSION = 'mdro-v180';
 const CACHE = CACHE_VERSION;
 
 const CORE_FILES = [
