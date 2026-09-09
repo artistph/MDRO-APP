9fc602cb4d606af0c4b6c584f4867c8e07de707f
artistph <ahmednumber199@gmail.com>
2026-09-09 16:40:07 +0300

    Tabs bar gap 5px (5px divider between menu square and first tab)

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 // â”€â”€ Caching â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-const CACHE_VERSION = 'mdro-v171';
+const CACHE_VERSION = 'mdro-v172';
 const CACHE = CACHE_VERSION;
 
 const CORE_FILES = [
